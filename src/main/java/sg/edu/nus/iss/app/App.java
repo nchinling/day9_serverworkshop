55c6f1c632b5aadddc00cffbeabbbf6c45fc592f
nchinling <nchinling@gmail.com>
2023-02-13 11:31:57 +0800

    With server

--- src/main/java/sg/edu/nus/iss/app/App.java
+++ src/main/java/sg/edu/nus/iss/app/App.java
@@ -1,44 +1,77 @@
 package sg.edu.nus.iss.app;
 
-
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 import java.util.Scanner;
 
+public final class App {
+    private App() {
+    }
 
+    public static void main(String[] args) throws IOException {
 
-
-public final class App 
-{   private App(){
-
-    }   
-    public static void main( String[] args )
-    {
+        // need the random class to carry out randomize operation
         Random random = new Random();
 
+        // generate random number between 0 and 99
         Integer randomNumber = random.nextInt(100);
 
-        //store my guess
+        // store my guess
         Integer myGuess = 0;
+        
+        // open the socket server to listen on port 1234 for input
+        System.out.println("Server running on port 1234...");
+        ServerSocket ss = new ServerSocket(1234);
+        Socket s = ss.accept();
 
-        //input from keyboard
-        //convert to expect from inputstream  if it's a socket app.
-        System.out.println("Enter a number:");
-        Scanner scanner = new Scanner(System.in);
+        // prepare input coming through socket from client (receiving)
+        InputStream is = s.getInputStream();
+        BufferedInputStream bis = new BufferedInputStream(is);
+        DataInputStream dis = new DataInputStream(bis);
 
-        //allow user to guess until tney got the random number correct. 
-        while(myGuess != randomNumber){
-            myGuess = scanner.nextInt();
+        // prepare sending data out using socket to client (sending out)
+        OutputStream os = s.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        DataOutputStream dos = new DataOutputStream(bos);
 
-            if(myGuess < randomNumber){
-                System.out.println("Your guess is lower. ");
+        String msgRecv = "";
+
+        while (!msgRecv.equals("quit")) {
+            // guess XX
+            msgRecv = dis.readUTF();
+
+            if (msgRecv.contains("guess")) {
+                myGuess = Integer.parseInt(msgRecv.substring(6));
             }
-            else if (myGuess > randomNumber){
-                System.out.println("Your guess is higher. ");
-            } 
-            else{
-                 System.out.println("It is correct! ");
-                 scanner.close();
+
+
+            if (myGuess < randomNumber) {
+                dos.writeUTF("Your guessed number is lower.");
+            } else if (myGuess > randomNumber) {
+                dos.writeUTF("Your guessed number is higher");
+            } else {
+                dos.writeUTF("you have finally guessed it right!");
             }
+
+            // ensure records are written and send across the socket 
+            dos.flush();
         }
+
+        // close the input and output streams
+        dos.close();
+        bos.close();
+        os.close();
+
+        dis.close();
+        bis.close();
+        is.close();
     }
 }
