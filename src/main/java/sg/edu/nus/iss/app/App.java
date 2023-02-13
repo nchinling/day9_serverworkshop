9670ae474841b01fe921b7c9bc9f447d2db7e842
nchinling <nchinling@gmail.com>
2023-02-13 10:58:34 +0800

    Guessing game local without socket

--- src/main/java/sg/edu/nus/iss/app/App.java
+++ src/main/java/sg/edu/nus/iss/app/App.java
@@ -1,13 +1,44 @@
 package sg.edu.nus.iss.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+
+public final class App 
+{   private App(){
+
+    }   
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Random random = new Random();
+
+        Integer randomNumber = random.nextInt(100);
+
+        //store my guess
+        Integer myGuess = 0;
+
+        //input from keyboard
+        //convert to expect from inputstream  if it's a socket app.
+        System.out.println("Enter a number:");
+        Scanner scanner = new Scanner(System.in);
+
+        //allow user to guess until tney got the random number correct. 
+        while(myGuess != randomNumber){
+            myGuess = scanner.nextInt();
+
+            if(myGuess < randomNumber){
+                System.out.println("Your guess is lower. ");
+            }
+            else if (myGuess > randomNumber){
+                System.out.println("Your guess is higher. ");
+            } 
+            else{
+                 System.out.println("It is correct! ");
+                 scanner.close();
+            }
+        }
     }
 }
